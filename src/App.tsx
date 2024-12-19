a048c8c0277e393d3398d0a3fd3a9c8128bdf90b
tami <tami@optatumplatform.com>
2024-12-19 11:23:57 +0900

    feat: input value

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import { useRecoilState } from "recoil";
-import { minuteState } from "./atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
 
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event?.currentTarget.value); // by putting + will change the type of the value (string -> number)
+    setMinutes(+event.currentTarget.value); // by putting + will change the type of the value (string -> number)
   };
 
   return (
     <>
       <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
-      <input type="number" placeholder="Hours" />
+      <input value={hours} type="number" placeholder="Hours" />
     </>
   );
 }
