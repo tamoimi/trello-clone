063a8d7706b825eb239ea80bfb8a5389adac93b1
tami <tami@optatumplatform.com>
2024-12-19 11:24:22 +0900

    feat: add RecoilRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { RecoilRoot } from "recoil";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </StrictMode>
 );
